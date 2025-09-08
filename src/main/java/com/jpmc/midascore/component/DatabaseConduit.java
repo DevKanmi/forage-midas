ae1fd6497af77dcab6faed25f5b5844966ffed5f
Akinbayo Clinton <clintexakinbs@gmail.com>
2025-09-08 04:01:29 +0100

    chore: update dbConduit to save transactions to db and Transactionlistner to make use of the dbconduit to persist transactions in-memory

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,19 +1,55 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DatabaseConduit {
     private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
 
-    public DatabaseConduit(UserRepository userRepository) {
+
+    //We did Constructor Injection here instead of Autowiring
+    public DatabaseConduit(UserRepository userRepository, TransactionRepository transactionRepository) {
         this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
     }
 
     public void save(UserRecord userRecord) {
         userRepository.save(userRecord);
+    }
+    public UserRecord findUserById(long id){
+        return userRepository.findById(id);
+    }
+
+    @Transactional
+    public boolean processTransaction(long senderId, long recipientId, float amount){
+        UserRecord sender = userRepository.findById(senderId);
+        UserRecord recipient = userRepository.findById(recipientId);
+
+        //Validate
+        if(sender == null || recipient == null) return false;
+
+        if(sender.getBalance() < amount) return false;
+
+        sender.setBalance(sender.getBalance() - amount);
+        recipient.setBalance(recipient.getBalance() + amount);
+
+        //Save updated balance
+        userRepository.save(sender);
+        userRepository.save(recipient);
+
+        TransactionRecord transaction = new TransactionRecord(sender, recipient, amount);
+        transactionRepository.save(transaction);
+
+        return true;
+
+
+
     }
 
 }
--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -3,6 +3,7 @@ package com.jpmc.midascore.component;
 import com.jpmc.midascore.foundation.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class TransactionListener {
     private static final Logger logger = LoggerFactory.getLogger(TransactionListener.class);
+
+    @Autowired
+    private DatabaseConduit databaseConduit;
 
     @Value("${general.kafka-topic}")
     private String topic;
@@ -23,6 +27,17 @@ public class TransactionListener {
         logger.info("*** RECEIVED TRANSACTION ***");
         logger.info("Received transaction: {}", transaction);
         logger.info("Transaction amount: {}", transaction.getAmount());
+
+        //Process Transaction through Database conduit
+        boolean success = databaseConduit.processTransaction(
+                transaction.getSenderId(),
+                transaction.getRecipientId(),
+                transaction.getAmount());
+        if(success) logger.info("Transaction has been saved to DB successfully.");
+        else{
+            logger.info("Failed to save Transaction to DB");
+        }
+
         logger.info("*** END TRANSACTION ***");
     }
 }
